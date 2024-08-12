818931aa02cadc74668cec74d61c82e4e8a2c653
christianjunior05 <bchrist.junior@gmail.com>
2024-08-12 01:13:31 +0000

    add

--- database/migrations/2024_08_12_010557_create_books_table.php
+++ database/migrations/2024_08_12_010557_create_books_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table ->string("nom");
+            $table->strng('description');
+            $table->unsignedInteger('prix');
             $table->timestamps();
         });
     }
