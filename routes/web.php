cdfb6baf788ac81f2fd4acd00579f7011cb3260c
christianjunior05 <bchrist.junior@gmail.com>
2024-08-17 11:13:27 +0000

    add

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/author/create', [AuthorController::class, 'create'])->name('author.create');
-Route::get('/author/index', [AuthorController::class, 'index'])->name('author.create');
+Route::get('/author/index', [AuthorController::class, 'index'])->name('author.index');
 Route::post('/author', [AuthorController::class, 'store'])->name('author.store');
