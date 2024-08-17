ed4017cdc3a8653e6cca3197eb5ff0cd72a70f1d
christianjunior05 <bchrist.junior@gmail.com>
2024-08-17 22:45:38 +0000

    destroy

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -14,5 +14,7 @@ class Author extends Model
     public function books()
     {
         return $this->hasMany(Book::class);
+   
+        return $this->hasMany(Book::class)->onDelete('cascade');
     }
 }
