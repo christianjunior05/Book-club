65f6c71d8475410e525a11a26767dbbd0c316d31
christianjunior05 <bchrist.junior@gmail.com>
2024-08-17 11:15:08 +0000

    add

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Book;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Author extends Model
 {
     use HasFactory;
     protected $fillable = ['name_author'];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
